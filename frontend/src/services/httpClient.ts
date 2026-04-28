2ddec578f17a4dea1e2020238752a778671da58f
pinstripezebra <seelcs12@gmail.com>
2026-04-28 15:45:01 -0700

    updating delete image function

--- frontend/src/services/httpClient.ts
+++ frontend/src/services/httpClient.ts
@@ -54,6 +54,10 @@ export class HttpClient {
       throw new Error(errorMessage);
     }
 
+    if (response.status === 204 || response.headers.get('content-length') === '0') {
+      return undefined as unknown as T;
+    }
+
     return await response.json();
   }
 
